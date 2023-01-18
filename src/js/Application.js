eab80629865f25cf48e5b2c565456e8a40820c3c
Arif <arifkutq63@gmail.com>
2023-01-18 20:52:06 +0200

    work

--- src/js/Application.js
+++ src/js/Application.js
@@ -2,6 +2,7 @@ import EventEmitter from "eventemitter3";
 import Beat from "./Beat";
 
 export default class Application extends EventEmitter {
+
   static get events() {
     return {
       READY: "ready",
@@ -11,18 +12,18 @@ export default class Application extends EventEmitter {
   constructor() {
     super();
     this._beat = new Beat();
-    this._count = 0;
-    this._lyrics = ["Ah", "ha", "ha", "ha", "stayin' alive", "stayin' alive"];
-    this._create();
+     this.lyrics = ["Ah", "ha", "ha", "ha", "stayin' alive", "stayin' alive"];
+     this._create();
     this._beat.on(Beat.events.BIT, this._create);
     this.emit(Application.events.READY);
   }
 
   _create() {
-    const message = document.createElement("div");
-    message.classList.add("message");
-    message.innerText = this._lyrics[this._count];
-    document.querySelector(".main").appendChild(message);
-    this._count = (this._count + 1) % this._lyrics.length;
+    for (let i = 0; i < this.lyrics.length; i++) {
+      const message = document.createElement("div");
+      message.classList.add("message");
+      message.innerText = this.lyrics[i];
+      document.querySelector(".main").appendChild(message);
+    }
   }
 }
